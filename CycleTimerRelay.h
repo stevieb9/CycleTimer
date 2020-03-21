d6a2b613e7352fd280754a9f54fad452ff4ed2bc
Steve Bertrand <steveb@cpan.org>
2020-03-21 16:04:34 -0700

    Cleanup

--- CycleTimerRelay.h
+++ CycleTimerRelay.h
@@ -1,8 +1,6 @@
 #ifndef CYCLETIMERRELAY_H
 #define CYCLETIMERRELAY_H
 
-#define DEBUG_TIMING true
-
 #include "Arduino.h"
 #include "../ArduinoRelay/ArduinoRelay.h"
 
@@ -16,22 +14,19 @@ class CycleTimerRelay : public ArduinoRelay {
         unsigned long _pMillis  = 0;
 
     public:
-        /* Relay pin; On time; Off time */
-        CycleTimerRelay(int8_t, unsigned long, unsigned long);
- 
-        /* No relay pin */
-        CycleTimerRelay(unsigned long, unsigned long);
-
-        void    process ();
+        CycleTimerRelay(int8_t pin, unsigned long onTime, unsigned long offTime);
+        CycleTimerRelay(unsigned long onTime, unsigned long offTime);
 
         unsigned long prevMillis () { return _pMillis; }
         unsigned long prevMillis (unsigned long millis) { _pMillis = millis; return _pMillis; }
 
         unsigned long onTime () { return _onTime; }
-        unsigned long onTime (unsigned long time) { _onTime = time; return _onTime; }
+        unsigned long onTime (unsigned long millis) { _onTime = millis; return _onTime; }
 
         unsigned long offTime () { return _offTime; }
-        unsigned long offTime (unsigned long time) { _offTime = time; return _offTime; }
+        unsigned long offTime (unsigned long millis) { _offTime = millis; return _offTime; }
+
+        void    process ();
 };
 
 #endif
